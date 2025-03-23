486cac994af86883db416865c642e1b6501a8fa1
K.K. Lamberty <lamberty@morris.umn.edu>
2025-03-23 01:48:27 -0500

    Make it easier to find the environment setting for deployment
    We need to put in the URL for our deployment for web sockets to work correctly. Basically, the only thing that must be done after cloning the repo and before building.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: '/api/',
+  // DEPLOYMENT: need to update the droplet ip
   wsUrl: 'wss://[your droplet ip address].nip.io/api/websocket',
 };
