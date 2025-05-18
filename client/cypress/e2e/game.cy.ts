53e6910b0cfd28402f5e43abf5642a715c53ce6c
K.K. Lamberty <lamberty@morris.umn.edu>
2025-05-18 13:56:17 -0500

    Update e2e test to verify updates from web socket message
    Update this test so it is checking a bit more about the list of players before and after the simulated behavior of receiving a web socket message about adding a player

--- client/cypress/e2e/game.cy.ts
+++ client/cypress/e2e/game.cy.ts
@@ -18,6 +18,9 @@ describe('Game', () => {
 
   it('Should update the list of players when one is added', () => {
     cy.task("connect");
+    // initially, Yvonne is not one of the players
+    page.getPlayers().should('not.include.text', 'Yvonne');
+    // send a websocket message like you would if another client added a new player 'Yvonne'
     cy.url().then((url) => {
       const aGameId = url.split("/").reverse()[0];
       cy.task('addPlayer', {
@@ -26,7 +29,10 @@ describe('Game', () => {
         playerName: 'Yvonne',
       })
     });
+    // this client (this page) should reflect that change (increased number of players)
     page.getPlayers().should('have.lengthOf', 5);
+    // Yvonne will show as a player (even though *this* client didn't initiate that change)
+    page.getPlayers().should('include.text', 'Yvonne');
   });
 
 })
