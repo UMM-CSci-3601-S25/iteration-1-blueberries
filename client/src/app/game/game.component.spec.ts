3a42a349e1a229d443a7fd207b4f90764ba975bc
K.K. Lamberty <lamberty@morris.umn.edu>
2025-06-02 03:20:01 -0500

    Add test about which game a gameComponent has
    Since GameComponent uses a signal, this looks a bit different from when we used paramMap in User. One consequence is that the detectChanges needs to move out of the beforeEach so that we can manipulate the GameComponent before we check it.

--- client/src/app/game/game.component.spec.ts
+++ client/src/app/game/game.component.spec.ts
@@ -6,23 +6,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GameService } from './game.service';
 import { MockGameService } from 'src/testing/game.service.mock';
 import { RouterModule } from '@angular/router';
+import { Game } from './game';
 
 describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
-  //let gameService: GameService;
+  const mockGameService = new MockGameService();
 
   beforeEach(() => {
-    TestBed.overrideProvider(GameService, { useValue: new MockGameService() });
     TestBed.configureTestingModule({
       imports: [
         GameComponent,
         MatListModule,
         BrowserAnimationsModule,
         RouterModule.forRoot([
-          { path: 'games/1', component: GameComponent}
+          { path: 'games/kkGameId', component: GameComponent}
         ]),
       ],
+      providers: [
+        { provide: GameService, useValue: mockGameService },
+      ]
     }).compileComponents().catch(error => {
       expect(error).toBeNull();
     });
@@ -31,15 +34,23 @@ describe('GameComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
-    //gameService = TestBed.inject(GameService);
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
   it('should have a game service', () => {
+    fixture.detectChanges();
     expect(component.gameService).toBeTruthy();
+  });
+
+  it('should navigate to a specific gameComponent', () => {
+    const expectedGame: Game = MockGameService.testGames[0];
+    // The game is a writable signal. We will set it and then check it.
+    fixture.componentRef.instance.game.set(expectedGame);
+    fixture.detectChanges();
+    expect(component.game()).toEqual(expectedGame);
   });
 });
